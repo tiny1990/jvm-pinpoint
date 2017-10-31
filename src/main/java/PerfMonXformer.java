b99bebf0f689b86a925fb4362a85d0ccbc63d77a
pmx <tiny1990@126.com>
2017-10-31 11:54:49 +0800

    first commit

---
+++ src/main/java/PerfMonXformer.java
@@ -0,0 +1,61 @@
+import java.io.ByteArrayInputStream;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtBehavior;
+import javassist.CtClass;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+
+public class PerfMonXformer implements ClassFileTransformer {
+  public byte[] transform(ClassLoader loader, String className,
+                          Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+                          byte[] classfileBuffer) throws IllegalClassFormatException {
+    byte[] transformed = null;
+    System.out.println("Transforming " + className);
+    ClassPool pool = ClassPool.getDefault();
+    CtClass cl = null;
+    try {
+      cl = pool.makeClass(new ByteArrayInputStream(
+          classfileBuffer));
+      if (!cl.isInterface()) {
+        CtBehavior[] methods = cl.getDeclaredBehaviors();
+        for (CtBehavior method : methods) {
+          if (!method.isEmpty()) {
+            if (method.getName().equals("main")) {
+              doMethod(method);
+            }
+          }
+        }
+        transformed = cl.toBytecode();
+      }
+    } catch (Exception e) {
+      System.err.println("Could not instrument  " + className
+          + ",  exception : " + e.getMessage());
+      e.printStackTrace();
+    } finally {
+      if (cl != null) {
+        cl.detach();
+      }
+    }
+    return transformed;
+  }
+
+  private void doMethod(CtBehavior method) throws CannotCompileException {
+    // method.insertBefore("long stime = System.nanoTime();");
+    // method.insertAfter("System.out.println(/"leave "+method.getName()+" and time:/"+(System.nanoTime()-stime));");
+    method.instrument(new ExprEditor() {
+      @Override
+      public void edit(MethodCall m) throws CannotCompileException {
+        m.replace("{long stime = System.nanoTime(); " +
+            "$_ = $proceed($$);    " +
+            "System.out.println(\"" + m.getClassName() + "." + m.getMethodName() + ":  \"+ (System.nanoTime()-stime)); }");
+      }
+    });
+  }
+
+
+}
+
